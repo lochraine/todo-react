2335d81637a65cad97f1536b471c1c6c0587f8c0
lochraine <lochraine@hotmail.co.uk>
2019-01-25 19:30:54 +0000

    added checked functionailty and fixed render error in react, by changing removeTodo from arrow function method, and invoking when passing to the component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Todo from './Components/Todo/Todo';
+import Todo1 from './Components/Todo/Todo1';
 
+let id = 0;
+const Todo = (props) => {
+  
 
+  
+    return(
+      
+    <li>
+      <input onChange={props.toggleTodo} type="checkbox" checked={props.todo.checked}/>
+      <button>delete</button>
+      <span>{props.todo.text}</span>
+    </li>
+    
+  );
+
+}
 class App extends Component {
   constructor() {
     super()
@@ -12,24 +27,50 @@ class App extends Component {
     }
   }
 
-NewTodo = () => {
-  const text = 'Hi'//document.prompt("new todo");
+newTodo =() => {
+  const text = prompt("new todo");
+
   this.setState({
-    todos: [...this.state.todos, {text: text}],
+    todos: [...this.state.todos, {text: text, id: id++, checked: false},],
   })
   
 }
+
+
+
+removeTodo(id) {
+  this.setState({
+    todos: this.state.todos.filter(todo => todo.id !== id)
+  })
+}
+
+toggleTodo(id) {
+  this.setState({
+    todos: this.state.todos.map(todo => {
+      if (todo.id !== id) return todo
+      return {id: todo.id,
+              text: todo.text,
+              checked: !todo.checked}
+    })
+  })
+}
+
+
   render() {
     return (
-      <div className="App">
-      <button OnClick={this.NewTodo} type="button">New todo</button>
+      <div>
+      <button onClick={this.newTodo} type="button">new todo</button>
         <ul>
-        {this.state.todos.map(todo => <Todo todo={todo} />)}
+        {this.state.todos.map((todo) => (
+          <Todo todo={todo}
+           onDelete={() => this.removeTodo(todo.id)}
+           toggleTodo={() => this.toggleTodo(todo.id)}
+            />))}
         </ul>
           
           
       </div>
-    );
+    )
   }
 }
 
